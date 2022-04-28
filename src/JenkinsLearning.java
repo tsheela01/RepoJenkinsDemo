63633b2cde748a2e13d3bfc25e55523d80145c38
Sheela <sheela.devi@onespan.com>
2022-04-28 14:02:26 -0400

    Adding new project

---
+++ src/JenkinsLearning.java
@@ -0,0 +1,7 @@
+class JenkinsLearning
+{
+	public static void main(String args[])
+	{
+		System.out.println("This is the demo file for Jenkins");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+class JenkinsDemo
+{
+	public static void main(String args[])
+	{
+		System.out.println("This is the demo file for Jenkins");
+	}
+}
